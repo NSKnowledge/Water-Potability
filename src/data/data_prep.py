84bceb21862840a081e101e06698373652d6e73c
wns-nehash <133336683+wns-nehash@users.noreply.github.com>
2024-12-16 17:05:26 +0530

    updated with mean values

--- src/data/data_prep.py
+++ src/data/data_prep.py
@@ -11,12 +11,12 @@ def load_data(filepath:str) ->pd.DataFrame:
     except Exception as e:
         raise Exception(f"Error loading data from {filepath}:{e}")
 
-def fill_missing_with_median(df):
+def fill_missing_with_mean(df):
     try:
         for column in df.columns:
             if df[column].isnull().any():
-                median_value = df[column].median()
-                df[column].fillna(median_value,inplace=True)
+                mean_value = df[column].mean()
+                df[column].fillna(mean_value,inplace=True)
         return df
     except Exception as e:
         raise Exception(f"Error Filling missing values with mean:{e}")
@@ -35,8 +35,8 @@ def main():
         train_data = load_data(os.path.join(raw_data_path,"train_data.csv"))
         test_data = load_data(os.path.join(raw_data_path,"test_data.csv"))
 
-        train_processed_data = fill_missing_with_median(train_data)
-        test_processed_data = fill_missing_with_median(test_data)
+        train_processed_data = fill_missing_with_mean(train_data)
+        test_processed_data = fill_missing_with_mean(test_data)
         
         os.makedirs(processed_data_path)        
         save_data(train_processed_data,os.path.join(processed_data_path, "train_processed.csv"))
